2b93b60609382e7b02ac856fd2e209eb535d8933
Zerve0 <zerve0@hotmail.com>
2019-10-22 01:35:03 +0900

    helper functions, bubble, selection completed

---
+++ sort.js
@@ -0,0 +1,77 @@
+const bubbleSort = (arr) => {
+    for (let i = arr.length; i > 0; i--) {
+        let swap = false;
+
+        for (let j = 0; j < i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+                swap = true;
+            }
+        }
+
+        if (!swap)
+            break;
+    }
+
+    return arr;
+}
+
+const selectionSort = (arr) => {
+    for (let i = 0; i < arr.length; i++) {
+        let lowIdx = i;
+
+        for (let j = i + 1; j < arr.length; j++) {
+            if (arr[j] < arr[lowIdx])
+                lowIdx = j;
+        }
+
+        if (i !== lowIdx)
+            [arr[lowIdx], arr[i]] = [arr[i], arr[lowIdx]];
+    }
+
+    return arr;
+}
+
+const insertionSort = (arr) => {
+    //todo
+
+    return arr;
+}
+
+const mergeSort = (arr) => {
+    //todo
+
+    return arr;
+}
+
+const quickSort = (arr) => {
+    //todo
+
+    return arr;
+}
+
+const radixSort = (arr) => {
+    //todo
+
+    return arr;
+}
+
+//HELPER FUNCTIONS
+
+const generateArray = (length, max) => {
+    return Array.from({ length }, () => Math.floor(Math.random() * max))
+}
+
+const doSort = (sortFunc, algoName, arr) => {
+    console.log(algoName + ": ");
+    console.log("Unsorted: " + arr);
+    console.log("Sorted: " + sortFunc(arr));
+    console.log("----------");
+}
+
+// doSort(bubbleSort, 'Bubble Sort', generateArray(30, 100));
+//doSort(selectionSort, 'Selection Sort', generateArray(30, 100));
+doSort(insertionSort, 'Insertion Sort', generateArray(30, 100));
+// doSort(mergeSort, 'Merge Sort', generateArray(30, 100));
+// doSort(quickSort, 'Quick Sort', generateArray(30, 100));
+// doSort(radixSort, 'Radix Sort', generateArray(30, 1000));
